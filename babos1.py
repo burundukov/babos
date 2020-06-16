3d8901c5f770b16a9b287db590b5ef4ee08319d4
Harry <57965433+burundukov@users.noreply.github.com>
2020-06-16 16:37:25 +0300

    Added the working file view.py

--- babos1.py
+++ babos1.py
@@ -1,3 +1,53 @@
+
+
+def calculate(check):
+  summ = 0
+  for i in check:
+    summ += i
+
+  first = summ/n
+  print('Средний чек ' + str(first) + ' рублей')
+
+  person1 = []
+  person2 = []
+  for i in check:
+    if i - first >= 0:
+      person1.append(i - first)
+      person2.append(0)
+    else:
+      person1.append(0)
+      person2.append(first - i)
+
+  # person1 - те, кто тртил деньги (имеет чек)
+  # person2 - те, кто не имеет чека
+  # person0 - Имена участников
+
+  #Процесс вычисления:
+
+  result = []
+
+  for i in range(len(person1)):
+    if person1[i] != 0:
+      for j in range(len(person2)):
+        if person2[j] == 0:
+          continue
+        else:
+          delta = person1[i] - person2[j]
+          if delta < 0:
+            result.append('Человек ' + str(person0[j]) + ' платит человеку ' + str(person0[i]) + ' ' + str(person1[i]) + ' рублей')
+            person2[j] = - delta
+            person1[i] = 0
+            break
+          else:
+            result.append('Человек ' + str(person0[j]) + ' платит человеку ' + str(person0[i]) + ' ' + str(person2[j]) + ' рублей')
+            person2[j] = 0
+            person1[i] = delta
+    else:
+      continue
+
+  for i in result:
+    print(i)
+
 print('Введите количество человек')
 n = int(input())
 
@@ -15,50 +65,4 @@ for i in range(n):
   check.append(b)
 
 
-summ = 0
-for i in check:
-  summ += i
-
-first = summ/n
-print('Средний чек ' + str(first) + ' рублей')
-
-person1 = []
-person2 = []
-for i in check:
-  if i - first >= 0:
-    person1.append(i - first)
-    person2.append(0)
-  else:
-    person1.append(0)
-    person2.append(first - i)
-
-# person1 - те, кто тртил деньги (имеет чек)
-# person2 - те, кто не имеет чека
-# person0 - Имена участников
-
-#Процесс вычисления:
-
-result = []
-
-for i in range(len(person1)):
-  if person1[i] != 0:
-    for j in range(len(person2)):
-      if person2[j] == 0:
-        continue
-      else:
-        delta = person1[i] - person2[j]
-        if delta < 0:
-          result.append('Человек ' + str(person0[j]) + ' платит человеку ' + str(person0[i]) + ' ' + str(person1[i]) + ' рублей')
-          person2[j] = - delta
-          person1[i] = 0
-          break
-        else:
-          result.append('Человек ' + str(person0[j]) + ' платит человеку ' + str(person0[i]) + ' ' + str(person2[j]) + ' рублей')
-          person2[j] = 0
-          person1[i] = delta
-  else:
-    continue
-
-for i in result:
-  print(i)
-
+calculate(check)
--- view.py
+++ view.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 root = Tk()
-root.geometry('500x400+200+100')
+root.geometry('420x400+200+100')
 root.title('Babos')
 
 person0 = []
@@ -41,15 +41,72 @@ def finished():
     cash_entry.delete(0, END)
     out_label.config(text = consol)
 
-button1=Button(root, text = 'Подсчитать', bg = 'pink')
+    name_entry.focus_set()
+
+def calculate():
+  summ = 0
+  for i in check:
+    summ += i
+
+  first = summ/len(check)
+  print('Средний чек ' + str(first) + ' рублей')
+
+  person1 = []
+  person2 = []
+  for i in check:
+    if i - first >= 0:
+      person1.append(i - first)
+      person2.append(0)
+    else:
+      person1.append(0)
+      person2.append(first - i)
+
+  # person1 - те, кто тртил деньги (имеет чек)
+  # person2 - те, кто не имеет чека
+  # person0 - Имена участников
+
+  #Процесс вычисления:
+
+  result = []
+
+  for i in range(len(person1)):
+    if person1[i] != 0:
+      for j in range(len(person2)):
+        if person2[j] == 0:
+          continue
+        else:
+          delta = person1[i] - person2[j]
+          if delta < 0:
+            result.append('Человек ' + str(person0[j]) + ' платит человеку ' + str(person0[i]) + ' ' + str(person1[i]) + ' рублей')
+            person2[j] = - delta
+            person1[i] = 0
+            break
+          else:
+            result.append('Человек ' + str(person0[j]) + ' платит человеку ' + str(person0[i]) + ' ' + str(person2[j]) + ' рублей')
+            person2[j] = 0
+            person1[i] = delta
+    else:
+      continue
+
+  consol_2 = f"Средний чек {first} рублей" + '\n' + '\n' + '\n'
+  for i in result:
+    consol_2 += (i + '\n')
+  out_label.config(text=consol_2)
+
+def clear():
+    person0.clear()
+    check.clear()
+    consol = ""
+
+button1 = Button(root, text = 'Подсчитать', bg = 'pink', command = calculate)
 button1.grid(row=0, column=1)
 
-#button1.bind('<Button-1>', added)
-
-
-
 button2 = Button(root, text='внести данные', bg = 'yellow', command = finished)
+root.bind('<Return>', lambda event: finished())
 button2.grid(row=3, column=1)
+
+button_3 = Button(root, text = "Сброс", bg = 'pink', command = clear)
+button_3.grid(row = 0, column = 0)
 
 out_label = Label(wraplength=250)
 out_label.grid(row=4, column=1, padx=5, pady=5)
