194c5f3b632856f584937e52165dba74bf4f4b96
Harry <57965433+burundukov@users.noreply.github.com>
2020-06-29 16:11:16 +0300

    finished

--- view.py
+++ view.py
@@ -1,14 +1,16 @@
 from tkinter import *
+from tkinter.messagebox import showinfo
+
 
 root = Tk()
 root.iconbitmap('icon.ico')
 root.geometry('420x400+200+100')
 root.title('Babos')
 
-person0 = []
-check = []
+person0 = [] #список, в который заносятся имена участников
+check = [] #список, в который заносятся суммы трат
 
-person_out = []     #Список для отображения введенных данных
+person_out = [] #Список для отображения введенных данных
 
 name = StringVar()
 cash = StringVar()
@@ -25,15 +27,16 @@ name_entry.grid(row=1, column=1, padx=5, pady=5)
 cash_entry.grid(row=2, column=1, padx=5, pady=5)
 
 
-def finished():
+def insert():
 
     global consol
     consol = ""
-    check.append(float(cash.get()))
-    person0.append(name.get())
+    if name.get() in person0:
+        check[-1] += float(cash.get())
+    else:
+        person0.append(name.get())
+        check.append(float(cash.get()))
     person_out.append(name.get() + " заплатил " + cash.get())
-    print(person0)
-    print(check)
 
     for i in person_out:
         consol += (i + '\n')
@@ -51,7 +54,6 @@ def calculate():
     summ += i
 
   first = summ/len(check)
-  print('Средний чек ' + str(first) + ' рублей')
 
   person1 = []
   person2 = []
@@ -101,16 +103,24 @@ def clear():
     person_out.clear()
     out_label.config(text=consol)
 
+information = 'Здравствуйте. Вы пользуетесь программой для расчета среднего чека. Для каких случаев данная программа полезна?   Например, Вы с друзьями в семером здорово провели день вместе - сходили в кино, купили еды и напитков в кино, посидели в двух разных кафе и сходили поиграть в страйкбол. Прекрассный день! Но платил за все не один человек, а в нашем слечае четверо. Чтобы посчитать кто кому сколько должен после такого дня достаточно сложная процедура. И поэтому Вам на помощь придет эта программа.   Для расчетов введите имя каждого человека, и сколько он потратил. Даже если кто-то заплатил за всех в двух разных местах, просто введите его имя и потраченную сумму дважды. В таком случае следите за одинаковым написанием имени этого человека. Если вы хотите ввести не целое число, то вместо запятой используйте точку. Приятного пользования!'
+
+def info():
+    showinfo(title = "Информация", message = information)
+
 
 button1 = Button(root, text = 'Подсчитать', bg = 'pink', command = calculate)
 button1.grid(row=0, column=1)
 
-button2 = Button(root, text='внести данные', bg = 'yellow', command = finished)
-root.bind('<Return>', lambda event: finished())
+button2 = Button(root, text='внести данные', bg = 'yellow', command = insert)
+root.bind('<Return>', lambda event: insert())
 button2.grid(row=3, column=1)
 
 button_3 = Button(root, text = "Сброс", bg = 'pink', command = clear)
 button_3.grid(row = 0, column = 0)
+
+button_4 = Button(root, text = "Информация", bg = 'gray', command = info)
+button_4.grid(row = 3, column = 0)
 
 out_label = Label(wraplength=250)
 out_label.grid(row=4, column=1, padx=5, pady=5)
